f3dc76164bad489b23201d1ee6030e0fb6d23c1d
z920804tw <129352148+z920804tw@users.noreply.github.com>
2024-10-17 10:50:19 +0800

    2024/10/17
    1.新增了拿起物件和放置物件的功能
    2.匯入Outline顯示物件外框功能以及新增十字準心

---
+++ Assets/Script/PickController.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PickController : MonoBehaviour
+{
+    [Header("參數設定")]
+    public Transform handPos;
+    public LayerMask canPickLayer, placeLayer;
+    public float maxRayDistance;
+    public Image crossHairs;
+
+    [Header("Debug")]
+    [SerializeField] GameObject pickObject;
+    [SerializeField] bool isHolding;
+    Outline hitObject;
+    Collider col;
+    GameObject cam;
+    PlayerController playerController;
+
+
+    void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+        cam = playerController.PlayerCam;
+
+        col = GetComponent<Collider>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PickAndPlace();
+
+    }
+    void DetectObject()     //偵測可拿起物件和地板
+    {
+        Ray detcectRay = new Ray(cam.transform.position, cam.transform.forward);
+        RaycastHit hit;
+
+        if (isHolding == false)
+        {
+            if (Physics.Raycast(detcectRay, out hit, maxRayDistance, canPickLayer))
+            {
+                crossHairs.color = Color.green;
+                hitObject = hit.collider.GetComponent<Outline>();
+                hitObject.enabled = true;
+            }
+            else
+            {
+                if (hitObject != null)
+                {
+                    hitObject.enabled=false;
+                    hitObject=null;
+                }
+                crossHairs.color = Color.white;
+            }
+        }
+        else
+        {
+            if (Physics.Raycast(detcectRay, out hit, maxRayDistance, placeLayer))
+            {
+                crossHairs.color = Color.yellow;
+            }
+            else
+            {
+                crossHairs.color = Color.white;
+            }
+        }
+        Debug.DrawRay(cam.transform.position, cam.transform.forward * maxRayDistance, Color.red);
+
+    }
+    void PickAndPlace()
+    {
+        DetectObject();
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+            RaycastHit hit;
+            if (isHolding == false)
+            {
+                if (Physics.Raycast(ray, out hit, maxRayDistance, canPickLayer))
+                {
+                    pickObject = hit.collider.gameObject;
+                    pickObject.transform.SetParent(handPos);
+                    pickObject.transform.position = handPos.transform.position;
+                    pickObject.transform.localEulerAngles = new Vector3(0, 0, 0);
+                    pickObject.GetComponent<Rigidbody>().isKinematic = true;
+
+                    hitObject.enabled=false;
+                    Physics.IgnoreCollision(pickObject.GetComponent<Collider>(), col, true);
+
+                    isHolding = true;
+                }
+            }
+            else
+            {
+                if (Physics.Raycast(ray, out hit, maxRayDistance, placeLayer))
+                {
+                    pickObject.transform.SetParent(null);
+                    pickObject.transform.position = new Vector3(hit.point.x, hit.point.y + 0.2f, hit.point.z);
+                    pickObject.transform.rotation = Quaternion.identity;
+                    pickObject.GetComponent<Rigidbody>().isKinematic = false;
+                    Physics.IgnoreCollision(pickObject.GetComponent<Collider>(), col, false);
+
+                    pickObject = null;
+                    isHolding = false;
+                }
+            }
+        }
+    }
+}
